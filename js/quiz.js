0223786c9b864e2a9fdfdedde89167f18a7dc53d
patryfuentes85 <patry851105@gmail.com>
2022-03-06 18:10:07 +0100

    finish_validation

--- js/quiz.js
+++ js/quiz.js
@@ -1,32 +1,42 @@
 let questions = [];
-let correctAnswers =[];
+let correctAnswers = [];
 let count = 0;
+let results = { correct: 0,
+                incorrect: 0,
+                date: undefined
+              };
+
+
 
 const function1 = async function getQuestions() {
     let response = await fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`);
     let data = await response.json();
-    let final = await data.results.map( (item, index )=> {
-        questions.push({'question':item.question, 'correct_answer': item.correct_answer, 'incorrect_answers': item.incorrect_answers});
+    let final = await data.results.map((item, index) => {
+        questions.push({
+            'question': item.question,
+            'correct_answer': item.correct_answer,
+            'incorrect_answers': item.incorrect_answers
+        });
         correctAnswers.push(item.correct_answer);
     });
-    //console.log(correctAnswers);
+    //console.log(questions);
 }
 
-const function2 = async function test () {
-    
+const function2 = async function test() {
+
     let randomAnswers = questions.map((quest, index) => {
-      return [...questions[index].incorrect_answers, questions[index].correct_answer].sort(() => 0.5 - Math.random())
+        return [...questions[index].incorrect_answers, questions[index].correct_answer].sort(() => 0.5 - Math.random())
     })
 
     const h2 = document.querySelector('h2');
     h2.innerText = `${questions[count].question}`;
 
     const span = document.querySelector('#question-number');
-    span.innerText = `${count+1}`
-    
+    span.innerText = `Question Number: ${count+1}`
+
     const inputs = document.querySelectorAll('.inputClass');
     inputs.forEach((item, index) => {
-        item.setAttribute('value',`${randomAnswers[count][index]}`);
+        item.setAttribute('value', `${randomAnswers[count][index]}`);
     })
 
     const labels = document.querySelectorAll('label');
@@ -36,7 +46,7 @@ const function2 = async function test () {
     count++;
 
 }
-   
+
 
 const mother = async () => {
     const a = await function1();
@@ -44,34 +54,57 @@ const mother = async () => {
 }
 mother()
 
+console.log(correctAnswers);
 
+const getDate = () => {
+    const date = new Date();
+    const date2 = `0${date.getDate()}/0${date.getMonth()}/${date.getFullYear()}`;
+    const time = `${date.getHours()}:${date.getMinutes()}`;
+    return [date2, time].join(' ')
+}
 
 const validation = event => {
 
     const radio = event.target.answerField.value;
 
-   if (correctAnswers[count-1] == radio){
-      alert('Respuesta Correcta');
+    if (radio == '') {
+        console.log('esta vacío');
+    } else if (correctAnswers[count - 1] == radio) {
+        results.correct += 1;
+        console.log('Respuesta Correcta');
 
-    } else {
+    } else if (correctAnswers[count - 1] !== radio) {
+        results.incorrect += 1;
         console.log('Respuesta Incorrecta');
     }
-}
+    console.log(radio);
+};
 
+// Deseleccionar Radios
+
+function unselect() {
+    const radio = document.querySelectorAll('.inputClass')
+    radio.forEach((x) => x.checked = false);
+};
 
 const form = document.querySelector('form');
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    if (count <= 9 ){
+    if (count <= 9) {
         validation(event);
         function2();
-        
+        unselect();
         //validation
     } else {
         validation(event);
-        count=0;
+        count = 0;
+        unselect();
+        results.date = getDate();
         console.log('Se acabó!')
+        console.log(results);
         // fin del juego 
     }
+
 
 });
